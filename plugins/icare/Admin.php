d3769b501aedb330af7de1f58cc03e611441bc35
Windy Sabtami AJ <windysabtami85@gmail.com>
2026-05-15 15:59:32 +0800

    amsal

--- plugins/icare/Admin.php
+++ plugins/icare/Admin.php
@@ -67,10 +67,21 @@ class Admin extends AdminModule
   {
 
     $no_rkm_medis = $this->core->getRegPeriksaInfo('no_rkm_medis',revertNoRawat($no_rawat));
+    $kd_dokter_bpjs = null;
     if($status == 'fktp') {
-      $kd_dokter_bpjs = $this->db('maping_dokter_pcare')->where('kd_dokter', $this->core->getRegPeriksaInfo('kd_dokter',revertNoRawat($no_rawat)))->oneArray()['kd_dokter_pcare'];
+      $mapping_dokter = $this->db('maping_dokter_pcare')
+        ->where('kd_dokter', $this->core->getRegPeriksaInfo('kd_dokter',revertNoRawat($no_rawat)))
+        ->oneArray();
+      if (is_array($mapping_dokter) && isset($mapping_dokter['kd_dokter_pcare'])) {
+        $kd_dokter_bpjs = $mapping_dokter['kd_dokter_pcare'];
+      }
     } else {
-      $kd_dokter_bpjs = $this->db('maping_dokter_dpjpvclaim')->where('kd_dokter', $this->core->getRegPeriksaInfo('kd_dokter',revertNoRawat($no_rawat)))->oneArray()['kd_dokter_bpjs'];
+      $mapping_dokter = $this->db('maping_dokter_dpjpvclaim')
+        ->where('kd_dokter', $this->core->getRegPeriksaInfo('kd_dokter',revertNoRawat($no_rawat)))
+        ->oneArray();
+      if (is_array($mapping_dokter) && isset($mapping_dokter['kd_dokter_bpjs'])) {
+        $kd_dokter_bpjs = $mapping_dokter['kd_dokter_bpjs'];
+      }
     }
     date_default_timezone_set('UTC');
     $tStamp = strval(time() - strtotime("1970-01-01 00:00:00"));
@@ -81,7 +92,7 @@ class Admin extends AdminModule
     }
     $data = [
       'param' => $this->core->getPasienInfo('no_peserta',$no_rkm_medis),
-      'kodedokter' => intval($kd_dokter_bpjs)
+      'kodedokter' => intval($kd_dokter_bpjs ?: 0)
     ];
     $data = json_encode($data);
     $signature = hash_hmac('sha256', $this->consid."&".$tStamp, $this->secretkey, true);
@@ -94,20 +105,29 @@ class Admin extends AdminModule
       $output = BpjsService::postAplicare($url, $data, $this->consid, $this->secretkey, $this->user_key, $tStamp);
     }
     $json = json_decode($output, true);
-    // echo $output;
-    $riwayat['url'] = '';
-    if($json['metaData']['code'] == '200') {
-      $stringDecrypt = stringDecrypt($key, $json['response']);
-      $decompress = '""';
-      if (!empty($stringDecrypt)) {
-        $decompress = \LZCompressor\LZString::decompressFromEncodedURIComponent(($stringDecrypt));
-        $riwayat = json_decode($decompress, true);
+    $riwayat_url = '';
+    $message = 'Gagal memuat riwayat iCare';
+
+    if (is_array($json) && isset($json['metaData']['code'])) {
+      if ((string)$json['metaData']['code'] === '200') {
+        $stringDecrypt = stringDecrypt($key, isset($json['response']) ? $json['response'] : '');
+        if (!empty($stringDecrypt)) {
+          $decompress = LZString::decompressFromEncodedURIComponent($stringDecrypt);
+          $riwayat = json_decode($decompress, true);
+          if (is_array($riwayat) && isset($riwayat['url'])) {
+            $riwayat_url = (string)$riwayat['url'];
+          }
+        }
+        $message = '200';
+      } else {
+        $message = isset($json['metaData']['message']) ? (string)$json['metaData']['message'] : 'Gagal memuat riwayat iCare';
       }
-      $message = $json['metaData']['code'];
-    } else {
-      $message = $json['metaData']['message'];
     }
-    echo $this->draw('riwayat.html', ['url' => htmlspecialchars_array($riwayat['url']), 'message' => htmlspecialchars_array($message)]);
+
+    echo $this->draw('riwayat.html', [
+      'url' => htmlspecialchars($riwayat_url, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'),
+      'message' => htmlspecialchars((string)$message, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8')
+    ]);
     exit();
   }
 
